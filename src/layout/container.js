045b1fa84509d189e53caf96e0bdbfaca7a9c25c
Christhopher Leão <christhopherleao@icloud.com>
2020-10-29 12:00:12 +0000

    container with flex

--- src/layout/container.js
+++ src/layout/container.js
@@ -1,8 +1,10 @@
 import styled from 'styled-components';
 
 export default styled.div`
+  display: flex;
+  flex: 1 1 auto;
+  flex-direction: column;
   margin-right: auto;
   margin-left: auto;
   max-width: ${({ theme }) => theme.containerWidth || '84rem'};
-  width: 100%;
 `;
